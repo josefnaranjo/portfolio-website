0705d78ef7fcaebf9e48e9e8cf625a403d34d415
josefnaranjo <jfnaappus@gmail.com>
2024-06-01 12:32:46 -0700

    "Added 'use client' directive to layout.tsx, wrapped Navbar with ThemeProvider, and refactored Navbar component with new styles and dark mode toggle."

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+'use client'
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
+import { ThemeProvider } from 'next-themes'
 
 export default function RootLayout({
   children,
@@ -14,8 +16,11 @@ export default function RootLayout({
       */}
       <head />
       <body>
-        < Navbar />
-        {children}</body>
+        <ThemeProvider enableSystem={true} attribute='class'>
+          < Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,18 +31,62 @@ const Navbar = () => {
     const [navbar, setNavbar] = useState(false)
 
   return (
-    <header>
-        <div>
+    <header className={`w-full mx-auto px-4 sm:px-20 bg-white last:fixed top-0 z-50 shadow dark:border-b dark:border-stone-600 ${
+        currentTheme === 'dark' ? 'bg-stone-900' : 'bg-white'
+    }`}>
+        <div className='justify-between md:items-center md:flex'>
            <div>
-                <div>
-                    <h2>Jose Francisco Naranjo</h2>
+                <div className='flex items-center justify-between'>
+                    <div className='md:py-5 md:block'>
+                        <h2 className='text-2xl font-bold'>Jose Francisco Naranjo</h2>
+                    </div>
+                    <div className='md:hidden'>
+                        <button onClick={() => setNavbar(!navbar)}>
+                            {navbar ? <IoMdClose size={30} /> :  <IoMdMenu size={30} />}
+                        </button>
+                    </div>
                 </div>
            </div>
            <div>
-                {NAV_ITEMS.map((item, index) => {
-                    return <a key={index}>{item.label}</a>
-                })}
-            </div> 
+                <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                        navbar ? 'block' : 'hidden'
+                }`}>
+                    <div className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
+                            {NAV_ITEMS.map((item, index) => {
+                                return <Link
+                                key={index}
+                                to={item.page}
+                                className={`block lg:inline-block ${
+                                    currentTheme === 'dark' ? 'text-neutral-100 hover:text-neutral-400' : 'text-neutral-900 hover:text-neutral-500'
+                                }`}
+                                activeClass='active'
+                                spy={true}
+                                smooth={true}
+                                offset={-100}
+                                duration={500}
+                                onClick={() => setNavbar(!navbar)}
+                                >{item.label}</Link>
+                            })}
+                            {
+                                currentTheme === "dark" ? (
+                                    <button 
+                                        onClick={() => setTheme("light")} 
+                                        className='bg-slate-100 p-2 rounded-xl'
+                                    >
+                                        <RiSunLine size={25} color='black'/>
+                                    </button>
+                                    ) : (
+                                    <button 
+                                        onClick={() => setTheme("dark")} 
+                                        className='bg-slate-100 p-2 rounded-xl'
+                                    >
+                                        <RiMoonFill size={25}/>
+                                    </button>
+                                )
+                            }
+                        </div> 
+                </div>
+           </div>
         </div>
     </header>
   )
