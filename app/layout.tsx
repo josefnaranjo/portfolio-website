69bc15187b59468fbfb719c35c49df717e4c22e6
josefnaranjo <jfnaappus@gmail.com>
2024-06-08 16:16:44 -0700

    "Updated title and favicon in Head component, added Footer component to Layout, modified Navbar component, removed @next/font dependency, and updated selfie.jpg image."

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import { ThemeProvider } from 'next-themes'
 import { useEffect } from 'react'
 
@@ -17,10 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>
+      <body className='dark:bg-stone-900'>
         <ThemeProvider enableSystem={true} attribute='class' defaultTheme="light">
           <Navbar />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,17 +30,18 @@ const NAV_ITEMS: Array<NavItem> = [
 export default function Navbar() {
   const { systemTheme, theme, setTheme } = useTheme()
   const currentTheme = theme === "system" ? systemTheme : theme
-  const pathname = usePathname()
   const [navbar, setNavbar] = useState(false)
 
     return (
         <header className='w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600'>
             <div className='justify-between md:items-center md:flex'>
                 <div>
-                    <div className='flex items-center justify-between'>
-                        <div className='md:py-5 md:block'>
-                            <h2 className='text-2xl font-bold'>Jose Francisco Naranjo</h2>
-                        </div>
+                    <div className='flex items-center py-2 justify-between'>
+                        <Link className='cursor-pointer' to="home">
+                            <div className='md:py-5 md:block'>
+                                <h2 className='text-2xl font-bold'>Jose Francisco Naranjo</h2>
+                            </div>
+                        </Link>
                         <div className='md:hidden'>
                             <button onClick={() => setNavbar(!navbar)}>
                                 {navbar ? <IoMdClose size={30} /> :  <IoMdMenu size={30} />}
